8f24e9290ec2821e73bcdb15084ccbecf2ffdb3f
David Coats <david@getsafetycone.com>
2022-01-05 12:43:48 -0800

    Initial implementation.

---
+++ source/R5T.T0107.X0001/Code/Bases/Extensions/ICommitMessageExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using R5T.T0107;
+using R5T.T0107.X0001;
+
+
+namespace System
+{
+    public static class ICommitMessageGeneratorExtensions
+    {
+        public static string InitialSourceStubCommit(this ICommitMessage _)
+        {
+            return CommitMessages.InitialSourceStubCommit;
+        }
+    }
+}
---
+++ source/R5T.T0107.X0001/Code/Constants/CommitMessages.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+
+namespace R5T.T0107.X0001
+{
+    public static class CommitMessages
+    {
+        public static string InitialSourceStubCommit => "Initial commit of source-stub.";
+    }
+}
