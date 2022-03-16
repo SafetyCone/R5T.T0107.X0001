ab7fbb2e700cd83b15378f5a27cc522170a7a5c3
David Coats <david@getsafetycone.com>
2022-03-16 15:28:36 -0700

    Add ICommitMessage values.

--- source/R5T.T0107.X0001/Code/Bases/Extensions/ICommitMessageExtensions.cs
+++ source/R5T.T0107.X0001/Code/Bases/Extensions/ICommitMessageExtensions.cs
@@ -8,6 +8,16 @@ namespace System
 {
     public static class ICommitMessageGeneratorExtensions
     {
+        public static string AddGitIgnoreFile(this ICommitMessage _)
+        {
+            return CommitMessages.AddGitIgnoreFile;
+        }
+
+        public static string AddInitialSolution(this ICommitMessage _)
+        {
+            return CommitMessages.AddInitialSolution;
+        }
+
         public static string InitialSourceStubCommit(this ICommitMessage _)
         {
             return CommitMessages.InitialSourceStubCommit;
--- source/R5T.T0107.X0001/Code/Constants/CommitMessages.cs
+++ source/R5T.T0107.X0001/Code/Constants/CommitMessages.cs
@@ -5,6 +5,8 @@ namespace R5T.T0107.X0001
 {
     public static class CommitMessages
     {
+        public static string AddGitIgnoreFile => "Add git-ignore file.";
+        public static string AddInitialSolution => "Add initial solution.";
         public static string InitialSourceStubCommit => "Initial commit of source-stub.";
     }
 }
