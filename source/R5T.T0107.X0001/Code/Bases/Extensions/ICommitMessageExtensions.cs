19cc48177f81465d4ee06bba50c36d58a452b2d3
davidcoats <david@getsafetycone.com>
2023-03-07 23:51:58 -0800

    Interim changes.

--- source/R5T.T0107.X0001/Code/Bases/Extensions/ICommitMessageExtensions.cs
+++ source/R5T.T0107.X0001/Code/Bases/Extensions/ICommitMessageExtensions.cs
@@ -8,16 +8,19 @@ namespace System
 {
     public static class ICommitMessageGeneratorExtensions
     {
+        [Obsolete("See R5T.Z0036.ICommitMessage.AddGitIgnoreFile")]
         public static string AddGitIgnoreFile(this ICommitMessage _)
         {
             return CommitMessages.AddGitIgnoreFile;
         }
 
+        [Obsolete("See R5T.Z0036.ICommitMessage.AddInitialSolution")]
         public static string AddInitialSolution(this ICommitMessage _)
         {
             return CommitMessages.AddInitialSolution;
         }
 
+        [Obsolete("See R5T.Z0036.ICommitMessage.InitialSourceStubCommit.")]
         public static string InitialSourceStubCommit(this ICommitMessage _)
         {
             return CommitMessages.InitialSourceStubCommit;
--- source/R5T.T0107.X0001/Code/Constants/CommitMessages.cs
+++ source/R5T.T0107.X0001/Code/Constants/CommitMessages.cs
@@ -3,10 +3,14 @@
 
 namespace R5T.T0107.X0001
 {
+    [Obsolete("See R5T.Z0036.ICommitMessage")]
     public static class CommitMessages
     {
+        [Obsolete("See R5T.Z0036.ICommitMessage.AddGitIgnoreFile")]
         public static string AddGitIgnoreFile => "Add git-ignore file.";
+        [Obsolete("See R5T.Z0036.ICommitMessage.AddInitialSolution")]
         public static string AddInitialSolution => "Add initial solution.";
+        [Obsolete("See R5T.Z0036.ICommitMessage.InitialSourceStubCommit")]
         public static string InitialSourceStubCommit => "Initial commit of source-stub.";
     }
 }
